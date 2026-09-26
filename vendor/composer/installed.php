6b59d51c46f62e575c839c41980aae97f7a4ef8f
Oluwatimilehin <Oluwatimilehintawose2005@gmail.com>
2026-09-26 07:16:01 +0100

    Add Composer configuration and initial autoloading setup
    - Created composer.json for project metadata and autoloading configuration.
    - Added composer.lock to lock dependencies.
    - Implemented autoload.php, autoload_psr4.php, and related files for PSR-4 autoloading.
    - Introduced ClassLoader and InstalledVersions classes for managing class loading and installed packages.
    - Added LICENSE file with MIT license information.
    - Created autoload_classmap.php for class mapping.
    - Initialized installed.json and installed.php for tracking installed packages.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'oluwatimilehin/app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9c61bd78f9e4d86cf45d381ddb29a7618d976c8f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'oluwatimilehin/app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9c61bd78f9e4d86cf45d381ddb29a7618d976c8f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
